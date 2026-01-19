3143814e03a4fa40e75c4b109887b78a495f47d4
IsraelRe136 <resendizisrael57@gmail.com>
2026-01-19 08:26:14 -0600

    Fix typo in HeroSection heading

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>ADVENTURE </h1>
+      <h1>ADVENTURES </h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
         <Button
